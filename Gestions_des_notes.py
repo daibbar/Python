faa1434420eb727881491fcdfeae96c71fd27b2f
Mohamed Daibbar <mdaibbar@e2r8p14.1337.ma>
2025-10-20 22:28:02 +0100

    add elmaki exo

--- Gestions_des_notes.py
+++ Gestions_des_notes.py
@@ -1,19 +1,16 @@
 
-notes = []
-nb_saisies = 0
-nb_valides = 0
-
-print("(1) Ajouter une note")
-print("(2) Ajouter plusieurs notes (séparées par des espaces)")
-print("(3) Statistiques (min, max, moyenne, nb d'admis ≥10)")
-print("(4) Afficher l’appréciation d’une note")
-print("(5) Réinitialiser toutes les notes")
-print("(q) Quitter\n")
-
-
-choix = input("entrer une choix:")
+#### DAIBBAR MOHAMED, devoir IID1 exercice 06
+def menu():
+    print("(1) Ajouter une note")
+    print("(2) Ajouter plusieurs notes (séparées par des espaces)")
+    print("(3) Statistiques (min, max, moyenne, nb d'admis ≥10)")
+    print("(4) Afficher l’appréciation d’une note")
+    print("(5) Réinitialiser toutes les notes")
+    print("(q) Quitter\n")
 
 def statistiques(une_liste):
+        nb_admis = 0
+
         if (len(une_liste) == 0): 
             print("la liste est vide")
             return
@@ -22,29 +19,42 @@ def statistiques(une_liste):
 
         match choix:
             case "1":
-                print(f"le min is {} ")
-        
+                print(f"le min est: {min(notes)} ")
+            case "2":
+                print(f"le max est: {max(notes)}")
+            case "3":
+                print(f"la moyenne est: {round((sum(notes) / len(notes)), 2)}")
+            case "4":
+                i = 0
+                while (i < len(une_liste)):
+                    if (une_liste[i] >= 10): nb_admis += 1
+                    i += 1
+                print(f"le nombre des admis est: {nb_admis}\n")
 
-match choix:
-    case "1":
-        n = int(input("entrer la note: "))
-        if n > 20 or n < 0 :
-            print("votre choix n'est pas valide!")
-        else:
-            notes.append(n)
-    case "2":
-        notes.extend(list(map(int, input("Enter numbers: ").split())))
-    case "3":
-        statistiques(notes)
-    case "5":
-        notes = []
-    case "4":
-        n = int(input("entrer une note: "))
-        if (n >= 16) : print("Tres Bien!")
-        elif n >= 14 and n < 16 : print("Bien!")
-        elif n>= 12 and n <= 14 : print("Assez Bien!")
-        elif n >= 10 and n < 12 : print("Passable!")
-        else : print("Passable!")
-
-    
+notes = []
+while (1):
+    menu()
+    choix = input("entrer une choix:")
+    match choix:
+        case "1":
+            n = int(input("entrer la note: "))
+            if n > 20 or n < 0 :
+                print("votre choix n'est pas valide!")
+            else:
+                notes.append(n)
+        case "2":
+                notes.extend(list(map(int, input("Enter numbers: ").split())))
+        case "3":
+                statistiques(notes)
+        case "4":
+            n = int(input("entrer une note: "))
+            if (n >= 16) : print("Tres Bien!")
+            elif n >= 14 and n < 16 : print("Bien!")
+            elif n>= 12 and n <= 14 : print("Assez Bien!")
+            elif n >= 10 and n < 12 : print("Passable!")
+            else : print("Passable!")
+        case "5":
+                notes = []
+        case "q":
+                break
      
