0b09491e372d6b1bb3a8bcdbf979fcbff291c5c1
universe <daibbar.mohamed@gmail.com>
2026-02-16 17:01:59 +0000

    add py exo

---
+++ p00/ex1/ft_plot_area.py
@@ -0,0 +1,5 @@
+def ft_plot_area():
+    area_lenght = int(input("Enter length: "))
+    area_width = int(input("Enter width: "))
+    plot_area = area_lenght * area_width
+    print(f"Plot area: {plot_area}")
